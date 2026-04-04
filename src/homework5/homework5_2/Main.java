96050d3f65a23bd5e34d0b3dc4e1099d07eaa581
Kadirmyatova Anastasiya <kadyrmatovaanastasia12@gmail.com>
2026-04-04 19:41:05 +0300

    Добавление еще 2 фигур

--- src/homework5/homework5_2/Main.java
+++ src/homework5/homework5_2/Main.java
@@ -6,10 +6,13 @@ public class Main {
         Figures figures[] = {
                 new Triangle(3, 4, 5),
                 new Rectangle(3, 5),
-                new Circle(2)
+                new Circle(2),
+                new Circle(5),
+                new Triangle(3, 3, 3)
         };
 
-        double sumPerimeters = figures[0].perimeter() + figures[1].perimeter() + figures[2].perimeter();
-        System.out.println("Сумма периметров трех фигур: " + sumPerimeters);
+        double sumPerimeters = figures[0].perimeter() + figures[1].perimeter() + figures[2].perimeter()
+                + figures[3].perimeter() + figures[4].perimeter();
+        System.out.println("Сумма периметров пяти фигур: " + sumPerimeters);
     }
 }
